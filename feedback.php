94f8e943d98f3784bbb962c40d7a4d0fee756c58
oconpj18 <oconpj18+GitHub@gmail.com>
2016-08-15 11:20:57 -0400

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -1,9 +1,10 @@
-<?php header('Content-Type: text/html; charset=utf-8');
+<?php header('Content-Type: text/html; charset=utf-8'); ?>
+<?php
 ini_set('display_errors', 0);
 ini_set('log_errors', 0);
 ini_set('error_reporting', E_ALL);
-
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
