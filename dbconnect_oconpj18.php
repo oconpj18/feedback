676cfc7e05617612e4aaf51892c4b5526632f0a5
oconpj18 <oconpj18+GitHub@gmail.com>
2016-08-30 20:22:42 -0400

    Add files via upload

---
+++ dbconnect_oconpj18.php
@@ -0,0 +1,12 @@
+<?
+include("./phpincs/p.inc");
+
+$linkID = mysql_connect("localhost", "oconpj18",  $pword);
+   if( $linkID == FALSE ){
+	   print "Connection to database failed. Please notify the site administrator. <br>";
+} //endif
+
+if (mysql_select_db("oconpj18" , $linkID) == FALSE){ 
+	   print "The specified database could not be selected.. Please notify the site administrator. <br>";
+} //endif 
+?>
